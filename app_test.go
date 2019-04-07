621f03b109f94c114d127307c2eac640560d6902
DrPsychick <drpsychick@drsick.de>
2019-04-08 00:05:27 +0200

    Fix pointer overwrite (#16)
    * fix SSML (pointer overwrite)

--- app_test.go
+++ app_test.go
@@ -1,6 +1,7 @@
 package alfalfa
 
 import (
+	//"fmt"
 	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/l10n"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -12,14 +13,19 @@ func TestApplicationHelp(t *testing.T) {
 
 	l, err := l10n.Resolve("de-DE")
 	assert.Nil(t, err, "could not resolve locale 'de-DE'!")
+	assert.Equal(t, "de-DE", l.Name)
 
 	title, text, ssmlText := a.SaySomething(l)
 	assert.NotEmpty(t, title, "'title' must not be empty")
 	assert.NotEmpty(t, text, "'text' must not be empty")
 	assert.NotEmpty(t, ssmlText, "'ssmlText' must not be empty")
+	//fmt.Printf("ssml text: %s\n", ssmlText)
 
 	title, text, ssmlText = a.SSMLDemo(l)
 	assert.NotEmpty(t, title)
 	assert.NotEmpty(t, text)
 	assert.NotEmpty(t, ssmlText)
+	//fmt.Printf("ssml text: %s\n", ssmlText)
+
+	//assert.Nil(t, l.Name)
 }
--- l10n/l10n.go
+++ l10n/l10n.go
@@ -1,6 +1,8 @@
 package l10n
 
-import "github.com/drpsychick/alexa-go-cloudformation-demo/pkg/l10n"
+import (
+	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/l10n"
+)
 
 const (
 	GreetingTitle     l10n.Key = "greeting_title"
@@ -15,9 +17,25 @@ const (
 )
 
 func init() {
-	for _, l := range []l10n.Locale{deDE, enUS, frFR} {
-		if err := l10n.Register(&l); err != nil {
-			panic("registration of locale failed")
-		}
+	// ERROR: since l gets overwritten, locale "switches" to the last one
+	//var locales = []l10n.Locale{
+	//	deDE, enUS, frFR,
+	//}
+	//for _, l := range locales {
+	//	fmt.Printf("Registering locale %s...\n", l.Name)
+	//	if err := l10n.Register(&l); err != nil {
+	//		panic("registration of locale failed")
+	//	}
+	//}
+	//l, _ := l10n.Resolve("en-US")
+	//fmt.Printf("German: %s\n", l.Name)
+	if err := l10n.Register(&deDE); err != nil {
+		panic("registration of locale failed")
+	}
+	if err := l10n.Register(&enUS); err != nil {
+		panic("registration of locale failed")
+	}
+	if err := l10n.Register(&frFR); err != nil {
+		panic("registration of locale failed")
 	}
 }
