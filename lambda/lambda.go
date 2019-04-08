1aaeb4ac8b7d4770539e1a3074961b13a7b64bf2
Nicholas Wiersma <nick@wiersma.co.za>
2019-04-08 15:56:40 +0200

    Created mux and default mux

--- lambda/lambda.go
+++ lambda/lambda.go
@@ -1,114 +1,82 @@
 package lambda
 
 import (
-	"fmt"
 	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/l10n"
 
 	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/alexa"
 )
 
 type Application interface {
-	//Handle()
 	Help() (string, string)
 	Stop(l *l10n.Locale) (string, string, string)
 	SSMLDemo(l *l10n.Locale) (string, string, string)
 	SaySomething(l *l10n.Locale) (string, string, string)
 }
 
-type Handler func(alexa.Request) (alexa.Response, error)
+func NewMux(app Application) alexa.Handler {
+	mux := alexa.NewServerMux()
 
-// HandleRequest is the lambda hander
-func HandleRequest(app Application) Handler {
-	return func(r alexa.Request) (alexa.Response, error) {
-		l, err := l10n.Resolve(string(r.Body.Locale))
-		if err != nil {
-			return alexa.Response{}, fmt.Errorf("could not resolve locale %s", string(r.Body.Locale))
-		}
+	mux.HandleRequestTypeFunc(alexa.TypeLaunchRequest, handleLaunch)
 
-		if r.Body.Type == alexa.TypeLaunchRequest {
-			return handleLaunch(r), nil
-		}
-		if r.Body.Type == alexa.TypeIntentRequest {
-			name := r.Body.Intent.Name
+	mux.HandleIntent(alexa.HelpIntent, handleHelp(app))
+	mux.HandleIntent(alexa.CancelIntent, handleStop(app))
+	mux.HandleIntent(alexa.StopIntent, handleStop(app))
 
-			switch name {
-			case alexa.HelpIntent:
-				title, text := app.Help()
-				return alexa.NewSimpleResponse(title, text), nil
+	mux.HandleIntent("SSMLDemoIntent", handleSSMLResponse(app))
+	mux.HandleIntent("SaySomething", handleSSMLResponse(app))
+	mux.HandleIntentFunc("DemoIntent", handleDemo)
 
-			case alexa.CancelIntent:
-				fallthrough
-			case alexa.StopIntent:
-				title, text, _ := app.Stop(l)
-				return alexa.NewSimpleResponse(title, text), nil
-
-			case "SSMLDemoIntent":
-				return handleSSMLResponse(app.SSMLDemo(l)), nil
-
-			case "SaySomething":
-				return handleSSMLResponse(app.SaySomething(l)), nil
-
-			case "DemoIntent":
-				return handleDemo(r), nil
-			}
-			return alexa.Response{}, fmt.Errorf("server: unknown intent %s", name)
-		}
-		if r.Body.Type == alexa.TypeSessionEndedRequest {
-
-		}
-		if r.Body.Type == alexa.TypeCanFulfillIntentRequest {
-
-		}
-		return alexa.Response{}, fmt.Errorf("server: unknown intent type %s", r.Body.Type)
-	}
+	return mux
 }
 
-func handleSimpleResponse(title string, text string) alexa.Response {
-	return alexa.NewSimpleResponse(title, text)
-}
-
-func handleSSMLResponse(title string, text string, ssmlText string) alexa.Response {
-	r := alexa.NewSimpleResponse(title, text)
-	r.Body.OutputSpeech.Type = "SSML"
-	r.Body.OutputSpeech.SSML = ssmlText
-	return r
-}
-
-func handleLaunch(request alexa.Request) alexa.Response {
+func handleLaunch(r *alexa.Request) alexa.Response {
 	title := "Launch"
 	//text := "Willkommen beim Karlsruhe Golang Meetup #3!"
 	text := "Ja?"
-	r := alexa.NewSimpleResponse(title, text)
-	r.Body.ShouldEndSession = false
-	return r
+
+	resp := alexa.NewSimpleResponse(title, text)
+	resp.Body.ShouldEndSession = false
+
+	return resp
 }
 
-func handleDemo(request alexa.Request) alexa.Response {
+func handleHelp(app Application) alexa.Handler {
+	return alexa.HandlerFunc(func(r *alexa.Request) alexa.Response {
+		title, text := app.Help()
+
+		return alexa.NewSimpleResponse(title, text)
+	})
+}
+
+func handleStop(app Application) alexa.Handler {
+	return alexa.HandlerFunc(func(r *alexa.Request) alexa.Response {
+		title, text, _ := app.Stop(r.Locale)
+		return alexa.NewSimpleResponse(title, text)
+	})
+}
+
+func handleSSMLResponse(app Application) alexa.Handler {
+	return alexa.HandlerFunc(func(r *alexa.Request) alexa.Response {
+		title, text, ssmlText := app.SSMLDemo(r.Locale)
+
+		resp := alexa.NewSimpleResponse(title, text)
+		resp.Body.OutputSpeech.Type = "SSML"
+		resp.Body.OutputSpeech.SSML = ssmlText
+		return resp
+	})
+}
+
+func handleDemo(r *alexa.Request) alexa.Response {
 	title := "Test"
 	text := "Pace ist geil!"
-	r := alexa.NewSimpleResponse(title, text)
-	r.Body.OutputSpeech.Type = "SSML"
-	r.Body.OutputSpeech.SSML = "<speak>" +
+
+	resp := alexa.NewSimpleResponse(title, text)
+	resp.Body.OutputSpeech.Type = "SSML"
+	resp.Body.OutputSpeech.SSML = "<speak>" +
 		"<voice name=\"Kendra\"><lang xml:lang=\"en-US\">" +
 		"<emphasis level=\"strong\">pace</emphasis>" +
 		"</lang></voice>" +
 		"<voice name=\"Marlene\">iss <emphasis level=\"strong\">geil!</emphasis></voice>" +
 		"</speak>"
-	return r
+	return resp
 }
-
-//func handleHello(request alexa.Request) alexa.Response {
-//	title := "Saying Hello"
-//	var text string
-//	switch request.Body.Locale {
-//	case alexa.LocaleAustralianEnglish:
-//		text = "G'day mate!"
-//	case alexa.LocaleGerman:
-//		text = "Hallo Welt"
-//	case alexa.LocaleJapanese:
-//		text = "こんにちは世界"
-//	default:
-//		text = "Hello, World"
-//	}
-//	return alexa.NewSimpleResponse(title, text)
-//}
--- pkg/alexa/server.go
+++ pkg/alexa/server.go
@@ -3,6 +3,8 @@ package alexa
 import (
 	"context"
 	"errors"
+	"fmt"
+	"sync"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/json-iterator/go"
@@ -10,14 +12,14 @@ import (
 
 // Handler represents an alexa request handler.
 type Handler interface {
-	Serve(*Request) (*Response, error)
+	Serve(*Request) Response
 }
 
 // HandlerFunc is an adapter allowing a function to be used as a handler.
-type HandlerFunc func(*Request) (*Response, error)
+type HandlerFunc func(*Request) Response
 
 // Serve serves the request.
-func (fn HandlerFunc) Serve(r *Request) (*Response, error) {
+func (fn HandlerFunc) Serve(r *Request) Response {
 	return fn(r)
 }
 
@@ -33,9 +35,9 @@ func (s *Server) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
 		return nil, err
 	}
 
-	resp, err := s.Handler.Serve(req)
-	if err != nil {
-		return nil, err
+	resp := s.Handler.Serve(req)
+	if resp.Error != nil {
+		return nil, resp.Error
 	}
 
 	return jsoniter.Marshal(resp)
@@ -51,4 +53,126 @@ func (s *Server) Serve() error {
 
 	lambda.StartHandler(s)
 	return nil
+}
+
+// Serve serves the given handler.
+func Serve(h Handler) error {
+	srv := &Server{Handler: h}
+
+	return srv.Serve()
+}
+
+// ServeMux is an Alexa request multiplexer.
+type ServeMux struct {
+	mu      sync.RWMutex
+	types   map[RequestType]Handler
+	intents map[string]Handler
+}
+
+// NewServeMux creates a new serve mux.
+func NewServerMux() *ServeMux {
+	return &ServeMux{
+		types:   map[RequestType]Handler{},
+		intents: map[string]Handler{},
+	}
+}
+
+// Handler returns the matched handler for a request, or an error.
+func (m *ServeMux) Handler(r *Request) (Handler, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	if h, ok := m.types[r.Body.Type]; ok {
+		return h, nil
+	}
+
+	if r.Body.Type != TypeIntentRequest {
+		return nil, fmt.Errorf("server: unknown intent type %s", r.Body.Type)
+	}
+
+	h, ok := m.intents[r.Body.Intent.Name]
+	if !ok {
+		return nil, fmt.Errorf("server: unknown intent %s", r.Body.Intent.Name)
+	}
+
+	return h, nil
+}
+
+// HandleRequestType registers the handler for the given request type.
+//
+// Any attempt to handle the IntentRequest type will be ignored, use Intent instead.
+func (m *ServeMux) HandleRequestType(requestType RequestType, handler Handler) {
+	if requestType == TypeIntentRequest {
+		return
+	}
+	if handler == nil {
+		panic("alexa: nil handler")
+	}
+
+	m.mu.Lock()
+
+	m.types[requestType] = handler
+
+	m.mu.Unlock()
+}
+
+// HandleRequestType registers the handler function for the given request type.
+//
+// Any attempt to handle the IntentRequest type will be ignored, use Intent instead.
+func (m *ServeMux) HandleRequestTypeFunc(requestType RequestType, handler HandlerFunc) {
+	m.HandleRequestType(requestType, handler)
+}
+
+// HandleIntent registers the handler for the given intent.
+func (m *ServeMux) HandleIntent(intent string, handler Handler) {
+	if handler == nil {
+		panic("alexa: nil handler")
+	}
+
+	m.mu.Lock()
+
+	m.intents[intent] = handler
+
+	m.mu.Unlock()
+}
+
+// HandleIntent registers the handler function for the given intent.
+func (m *ServeMux) HandleIntentFunc(intent string, handler HandlerFunc) {
+	m.HandleIntent(intent, handler)
+}
+
+// Serve serves the matched handler.
+func (m *ServeMux) Serve(r *Request) Response {
+	h, err := m.Handler(r)
+	if err != nil {
+		return NewErrorResponse(err)
+	}
+
+	return h.Serve(r)
+}
+
+var DefaultServeMux = NewServerMux()
+
+// HandleRequestType registers the handler for the given request type on the DefaultServeMux.
+//
+// Any attempt to handle the IntentRequest type will be ignored, use Intent instead.
+func HandleRequestType(requestType RequestType, handler Handler) {
+	DefaultServeMux.HandleRequestType(requestType, handler)
+}
+
+// HandleRequestType registers the handler function for the given request type on the DefaultServeMux.
+//
+// Any attempt to handle the IntentRequest type will be ignored, use Intent instead.
+func HandleRequestTypeFunc(requestType RequestType, handler HandlerFunc) {
+	DefaultServeMux.HandleRequestTypeFunc(requestType, handler)
+}
+
+// HandleIntent registers the handler for the given intent on the DefaultServeMux.
+func HandleIntent(intent string, handler Handler) {
+	DefaultServeMux.HandleIntent(intent, handler)
+}
+
+// HandleIntent registers the handler function for the given intent on the DefaultServeMux.
+func HandleIntentFunc(intent string, handler HandlerFunc) {
+	DefaultServeMux.HandleIntentFunc(intent, handler)
 }
