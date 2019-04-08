df4e5a0582976ff7e78f76a17d8bf0918a5b1e2d
DrPsychick <drpsychick@drsick.de>
2019-04-08 20:56:34 +0200

    fix "SaySomething" Intent

--- lambda/lambda.go
+++ lambda/lambda.go
@@ -23,7 +23,7 @@ func NewMux(app Application) alexa.Handler {
 	mux.HandleIntent(alexa.StopIntent, handleStop(app))
 
 	mux.HandleIntent("SSMLDemoIntent", handleSSMLResponse(app))
-	mux.HandleIntent("SaySomething", handleSSMLResponse(app))
+	mux.HandleIntent("SaySomething", handleSaySomethingResponse(app))
 	mux.HandleIntentFunc("DemoIntent", handleDemo)
 
 	return mux
@@ -68,6 +68,22 @@ func handleSSMLResponse(app Application) alexa.Handler {
 		}
 
 		title, text, ssmlText := app.SSMLDemo(l)
+
+		resp := alexa.NewSimpleResponse(title, text)
+		resp.Body.OutputSpeech.Type = "SSML"
+		resp.Body.OutputSpeech.SSML = ssmlText
+		return resp
+	})
+}
+
+func handleSaySomethingResponse(app Application) alexa.Handler {
+	return alexa.HandlerFunc(func(r *alexa.Request) alexa.Response {
+		l, err := l10n.Resolve(string(r.Body.Locale))
+		if err != nil {
+			return alexa.NewErrorResponse(err)
+		}
+
+		title, text, ssmlText := app.SaySomething(l)
 
 		resp := alexa.NewSimpleResponse(title, text)
 		resp.Body.OutputSpeech.Type = "SSML"
