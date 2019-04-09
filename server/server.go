48582ce69ca312efe8449677ae4d05f32ac1749c
Nicholas Wiersma <nick@wiersma.co.za>
2019-04-09 13:33:38 +0200

    Just get rid of server for now

--- server/server.go
+++ server/server.go
@@ -1,8 +1,6 @@
 package server
 
 import (
-	"fmt"
-
 	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/alexa"
 )
 
@@ -19,35 +17,35 @@ func NewServer(app Application) alexa.Handler {
 type Handler func(alexa.Request) (alexa.ResponseEnvelope, error)
 
 // HandleRequest is the lambda hander
-func HandleRequest(app Application) Handler {
-	return func(r alexa.Request) (alexa.ResponseEnvelope, error) {
-		name := r.Body.Intent.Name
-
-		switch name {
-		case "hello":
-			return handleHello(r), nil
-
-		case alexa.HelpIntent:
-			title, text := app.Help()
-			return alexa.NewSimpleResponse(title, text), nil
-		}
-
-		return alexa.ResponseEnvelope{}, fmt.Errorf("server: unknown intent %s", name)
-	}
-}
-
-func handleHello(request alexa.Request) alexa.ResponseEnvelope {
-	title := "Saying Hello"
-	var text string
-	switch request.Body.Locale {
-	case alexa.LocaleAustralianEnglish:
-		text = "G'day mate!"
-	case alexa.LocaleGerman:
-		text = "Hallo Welt"
-	case alexa.LocaleJapanese:
-		text = "こんにちは世界"
-	default:
-		text = "Hello, World"
-	}
-	return alexa.NewSimpleResponse(title, text)
-}
+//func HandleRequest(app Application) Handler {
+//	return func(r alexa.Request) (alexa.ResponseEnvelope, error) {
+//		name := r.Body.Intent.Name
+//
+//		switch name {
+//		case "hello":
+//			return handleHello(r), nil
+//
+//		case alexa.HelpIntent:
+//			title, text := app.Help()
+//			return alexa.NewSimpleResponse(title, text), nil
+//		}
+//
+//		return alexa.ResponseEnvelope{}, fmt.Errorf("server: unknown intent %s", name)
+//	}
+//}
+//
+//func handleHello(request alexa.Request) alexa.ResponseEnvelope {
+//	title := "Saying Hello"
+//	var text string
+//	switch request.Body.Locale {
+//	case alexa.LocaleAustralianEnglish:
+//		text = "G'day mate!"
+//	case alexa.LocaleGerman:
+//		text = "Hallo Welt"
+//	case alexa.LocaleJapanese:
+//		text = "こんにちは世界"
+//	default:
+//		text = "Hello, World"
+//	}
+//	return alexa.NewSimpleResponse(title, text)
+//}
