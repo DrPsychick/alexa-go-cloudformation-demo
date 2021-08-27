6c7bcf44a99523386277d97826123f732c309645
DrPsychick <drpsychick@drsick.de>
2021-08-27 23:39:49 +0200

    maint: cover lambda with more tests

--- lambda/lambda.go
+++ lambda/lambda.go
@@ -1,7 +1,6 @@
 package lambda
 
 import (
-	"errors"
 	"fmt"
 	"github.com/drpsychick/alexa-go-cloudformation-demo"
 	"github.com/drpsychick/alexa-go-cloudformation-demo/loca"
@@ -10,20 +9,6 @@ import (
 	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/alexa/l10n"
 	"github.com/hamba/pkg/log"
 	"github.com/hamba/pkg/stats"
-)
-
-const (
-	// SSMLDemoIntent is the name of the SSL demo intent
-	SSMLDemoIntent = "SSMLDemoIntent"
-	// SaySomethingIntent is the name of the say something intent
-	SaySomethingIntent = "SaySomething"
-	// DemoIntent is the name of the demo intent
-	DemoIntent = "DemoIntent"
-)
-
-var (
-	// ErrorLocaleNotFound is the error text for missing locale
-	ErrorLocaleNotFound = errors.New("locale not found")
 )
 
 // Application defines the interface used of the app
@@ -105,8 +90,16 @@ func handleEnd(app Application) alexa.HandlerFunc {
 			return
 		}
 
-		b.WithSpeech(loc.GetAny(loca.Stop)).
-			WithSimpleCard(loc.GetAny(loca.StopTitle), loc.GetAny(loca.Stop)).
+		title, text, _ := app.Stop(loc)
+
+		if len(loc.GetErrors()) > 0 {
+			handleLocaleErrors(b, loc.GetErrors())
+			loc.ResetErrors()
+			return
+		}
+
+		b.WithSpeech(text).
+			WithSimpleCard(title, text).
 			WithShouldEndSession(true)
 	})
 }
@@ -379,9 +372,14 @@ func handleAWSStatus(app Application, sb *gen.SkillBuilder) alexa.Handler {
 			default:
 				fallthrough
 			case alfalfa.ErrorNoTranslation:
+				if len(loc.GetErrors()) > 0 {
+					handleLocaleErrors(b, loc.GetErrors())
+					loc.ResetErrors()
+					return
+				}
 				resp = alfalfa.ApplicationResponse{}
 				resp.Title = loc.GetAny(l10n.KeyErrorNoTranslationTitle)
-				resp.Text = loc.GetAny(l10n.KeyErrorNoTranslationText, loca.SaySomething)
+				resp.Text = loc.GetAny(l10n.KeyErrorNoTranslationText, loca.AWSStatus)
 				resp.Speech = loc.GetAny(l10n.KeyErrorNoTranslationSSML)
 				resp.End = true
 			}
@@ -436,11 +434,13 @@ func localeDefaults(locale string) l10n.LocaleInstance {
 // handleMissingLocale makes Alexa respond with a "local not supported" error
 func handleMissingLocale(b *alexa.ResponseBuilder, locale string) {
 	b.WithSimpleCard("error", fmt.Sprintf("Locale '%s' is not supported!", locale)).
-		WithSpeech(l10n.Speak(l10n.UseVoiceLang("Kendra", "en-US", "Your language is not supported")))
+		WithSpeech(l10n.Speak(l10n.UseVoiceLang("Kendra", "en-US", "Your language is not supported"))).
+		WithShouldEndSession(true)
 }
 
 // handleLocaleErrors makes Alexa show the last error on the screen
 func handleLocaleErrors(b *alexa.ResponseBuilder, errs []error) {
 	b.WithSimpleCard("error", fmt.Sprintf("last error: %s", errs[len(errs)-1].Error())).
-		WithSpeech(l10n.Speak(l10n.UseVoiceLang("Kendra", "en-US", "An error occurred")))
+		WithSpeech(l10n.Speak(l10n.UseVoiceLang("Kendra", "en-US", "An error occurred"))).
+		WithShouldEndSession(true)
 }
--- lambda/lambda_test.go
+++ lambda/lambda_test.go
@@ -8,20 +8,228 @@ import (
 	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/alexa"
 	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/alexa/gen"
 	"github.com/drpsychick/alexa-go-cloudformation-demo/pkg/alexa/l10n"
-	"github.com/hamba/logger"
-	"github.com/hamba/statter/l2met"
+	"github.com/hamba/pkg/log"
+	"github.com/hamba/pkg/stats"
 	"github.com/stretchr/testify/assert"
-	"os"
 	"testing"
 )
 
+func initLocaleRegistry(t *testing.T) {
+	loca.Registry = l10n.NewRegistry()
+	err := loca.Registry.Register(&l10n.Locale{Name: "en-US", TextSnippets: l10n.Snippets{}})
+	assert.NoError(t, err)
+	loc, err := loca.Registry.Resolve("en-US")
+	assert.NoError(t, err)
+
+	loc.Set(l10n.KeyErrorNoTranslationTitle, []string{"error"})
+	loc.Set(l10n.KeyErrorNoTranslationText, []string{"Key '%s' not found!"})
+
+}
+
+func TestLambda_HandleLaunch(t *testing.T) {
+	initLocaleRegistry(t)
+
+	app := alfalfa.NewApplication(log.Null, stats.Null)
+	loc, err := loca.Registry.Resolve("en-US")
+	assert.NoError(t, err)
+	assert.NotEmpty(t, loc)
+
+	r := &alexa.RequestEnvelope{
+		Version: "1.0",
+		Request: &alexa.Request{
+			Locale: "de-DE",
+			Type:   alexa.TypeLaunchRequest,
+		},
+	}
+
+	sb := gen.NewSkillBuilder()
+	b := &alexa.ResponseBuilder{}
+	m := lambda.NewMux(app, sb)
+	m = middleware.WithRequestStats(m, app)
+
+	m.Serve(b, r)
+	resp := b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+
+	// now with locale
+	r.Request.Locale = "en-US"
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Contains(t, resp.Response.Card.Content, loca.LaunchText)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+
+	// now with loca
+	loc.Set(loca.LaunchTitle, []string{"Start"})
+	loc.Set(loca.LaunchText, []string{"Und los..."})
+	assert.NotEmpty(t, loc.Get(loca.LaunchTitle))
+	assert.NotEmpty(t, loc.GetAny(loca.LaunchText))
+
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, loc.Get(loca.LaunchTitle), resp.Response.Card.Title)
+}
+
+func TestLambda_HandleEnd(t *testing.T) {
+	initLocaleRegistry(t)
+
+	app := alfalfa.NewApplication(log.Null, stats.Null)
+
+	r := &alexa.RequestEnvelope{
+		Version: "1.0",
+		Request: &alexa.Request{
+			Locale: "de-DE",
+			Type:   alexa.TypeSessionEndedRequest,
+		},
+	}
+
+	sb := gen.NewSkillBuilder()
+	b := &alexa.ResponseBuilder{}
+	m := lambda.NewMux(app, sb)
+	m = middleware.WithRequestStats(m, app)
+
+	// missing locale
+	m.Serve(b, r)
+	resp := b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+
+	// with existing locale, but missing text
+	r.Request.Locale = "en-US"
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Contains(t, resp.Response.Card.Content, loca.Stop)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+
+	// with translations
+	loc, err := loca.Registry.Resolve("en-US")
+	assert.NoError(t, err)
+	loc.Set(loca.StopTitle, []string{"Stop"})
+	loc.Set(loca.Stop, []string{"Alright, it's over now."})
+
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, loc.Get(loca.StopTitle), resp.Response.Card.Title)
+	assert.Equal(t, loc.Get(loca.Stop), resp.Response.Card.Content)
+}
+
+func TestLambda_HandleHelp(t *testing.T) {
+	initLocaleRegistry(t)
+
+	app := alfalfa.NewApplication(log.Null, stats.Null)
+
+	r := &alexa.RequestEnvelope{
+		Version: "1.0",
+		Request: &alexa.Request{
+			Locale: "de-DE",
+			Type:   alexa.TypeIntentRequest,
+			Intent: alexa.Intent{
+				Name: alexa.HelpIntent,
+			},
+		},
+	}
+
+	sb := gen.NewSkillBuilder()
+	b := &alexa.ResponseBuilder{}
+	m := lambda.NewMux(app, sb)
+	m = middleware.WithRequestStats(m, app)
+
+	// missing locale
+	m.Serve(b, r)
+	resp := b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+
+	// with existing locale, but missing text
+	r.Request.Locale = "en-US"
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Contains(t, resp.Response.Card.Content, loca.Help)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+
+	// with translations
+	loc, err := loca.Registry.Resolve("en-US")
+	assert.NoError(t, err)
+	loc.Set(loca.HelpTitle, []string{"Help"})
+	loc.Set(loca.Help, []string{"I'd love to help you"})
+
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, loc.Get(loca.HelpTitle), resp.Response.Card.Title)
+	assert.Equal(t, loc.Get(loca.Help), resp.Response.Card.Content)
+}
+
+func TestLambda_HandleStop(t *testing.T) {
+	initLocaleRegistry(t)
+
+	app := alfalfa.NewApplication(log.Null, stats.Null)
+
+	r := &alexa.RequestEnvelope{
+		Version: "1.0",
+		Request: &alexa.Request{
+			Locale: "de-DE",
+			Type:   alexa.TypeIntentRequest,
+			Intent: alexa.Intent{
+				Name: alexa.StopIntent,
+			},
+		},
+	}
+
+	sb := gen.NewSkillBuilder()
+	b := &alexa.ResponseBuilder{}
+	m := lambda.NewMux(app, sb)
+	m = middleware.WithRequestStats(m, app)
+
+	// missing locale
+	m.Serve(b, r)
+	resp := b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+
+	// with existing locale, but missing text
+	r.Request.Locale = "en-US"
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+	assert.Contains(t, resp.Response.Card.Content, loca.Stop)
+
+	// with translations
+	loc, err := loca.Registry.Resolve("en-US")
+	assert.NoError(t, err)
+	loc.Set(loca.StopTitle, []string{"Stop"})
+	loc.Set(loca.Stop, []string{"Alright, it's over now."})
+
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, loc.Get(loca.StopTitle), resp.Response.Card.Title)
+	assert.Equal(t, loc.Get(loca.Stop), resp.Response.Card.Content)
+}
+
 func TestLambda_HandleSaySomething2(t *testing.T) {
-	// app with stdout logger and l2met stats
-	l := logger.New(logger.StreamHandler(os.Stdout, logger.LogfmtFormat()))
-	app := alfalfa.NewApplication(
-		l,
-		l2met.New(l, ""),
-	)
+	initLocaleRegistry(t)
+
+	app := alfalfa.NewApplication(log.Null, stats.Null)
+
 	r := &alexa.RequestEnvelope{
 		Version: "1.0",
 		Request: &alexa.Request{
@@ -41,77 +249,187 @@ func TestLambda_HandleSaySomething2(t *testing.T) {
 	resp := b.Build()
 
 	assert.NotEmpty(t, resp)
-	assert.NotEmpty(t, resp.Response.Card.Content)
-	//assert.NotEmpty(t, resp.Response.OutputSpeech.SSML)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+	assert.Equal(t, "Locale 'de-DE' is not supported!", resp.Response.Card.Content)
+
+	// with existing locale, but missing text
+	r.Request.Locale = "en-US"
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+	assert.Contains(t, resp.Response.Card.Content, "Error")
+
+	// with translations
+	loc, err := loca.Registry.Resolve("en-US")
+	assert.NoError(t, err)
+	loc.Set(loca.SaySomethingTitle, []string{"?"})
+	loc.Set(loca.SaySomethingText, []string{"Sadly, I have nothing to say."})
+	loc.Set(loca.SaySomethingSSML, []string{l10n.Speak(l10n.UseVoiceLang("Kendra", "en-US", "I like the Autobahn, it's so geil"))})
+
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, loc.Get(loca.SaySomethingTitle), resp.Response.Card.Title)
+	assert.Equal(t, loc.Get(loca.SaySomethingText), resp.Response.Card.Content)
 }
 
-func TestLambda_HandleSaySomething2_ErrorNoLocale(t *testing.T) {
-	loca.Registry = l10n.NewRegistry()
-	l := logger.New(logger.StreamHandler(os.Stdout, logger.LogfmtFormat()))
-	app := alfalfa.NewApplication(
-		l,
-		l2met.New(l, ""),
-	)
+func TestLambda_HandleAWSStatus(t *testing.T) {
+	initLocaleRegistry(t)
+
+	app := alfalfa.NewApplication(log.Null, stats.Null)
+
 	r := &alexa.RequestEnvelope{
+		Version: "1.0",
 		Request: &alexa.Request{
-			Locale: "en-US",
+			Locale: "de-DE",
 			Type:   alexa.TypeIntentRequest,
 			Intent: alexa.Intent{
-				Name: loca.SaySomething,
+				Name: loca.AWSStatus,
 			},
 		},
 	}
+
 	sb := gen.NewSkillBuilder()
 	b := &alexa.ResponseBuilder{}
 	m := lambda.NewMux(app, sb)
+	m = middleware.WithRequestStats(m, app)
 
+	// missing locale
 	m.Serve(b, r)
 	resp := b.Build()
 
+	assert.NotEmpty(t, resp)
 	assert.Equal(t, "error", resp.Response.Card.Title)
-	assert.Contains(t, resp.Response.Card.Content, "Locale 'en-US' is not supported!")
-}
 
-func TestLambda_HandleSaySomething2_ErrorNoTranslation(t *testing.T) {
-	err := loca.Registry.Register(&l10n.Locale{Name: "en-US", TextSnippets: l10n.Snippets{}})
-	assert.NoError(t, err)
+	// with existing locale, but missing text
+	r.Request.Locale = "en-US"
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, "error", resp.Response.Card.Title)
+	assert.Contains(t, resp.Response.Card.Content, loca.AWSStatusAreaElicitSSML)
+
+	// with translations
 	loc, err := loca.Registry.Resolve("en-US")
 	assert.NoError(t, err)
-	loc.Set(l10n.KeyErrorNoTranslationTitle, []string{"Translation error"})
-	loc.Set(l10n.KeyErrorNoTranslationText, []string{"No translation for '%s'"})
-	loc.Set(l10n.KeyErrorNoTranslationSSML, []string{l10n.Speak("I am missing translations.")})
-	l := logger.New(logger.StreamHandler(os.Stdout, logger.LogfmtFormat()))
-	app := alfalfa.NewApplication(
-		l,
-		l2met.New(l, ""),
-	)
+	loc.Set(loca.AWSStatusTitle, []string{"Status"})
+	//loc.Set(loca.AWSStatusText, []string{"Everything alright!"})
+	loc.Set(loca.AWSStatusAreaElicitText, []string{"Elicit Area"})
+	loc.Set(loca.AWSStatusAreaElicitSSML, []string{"<speak>Elicit Area<speak>"})
+
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, loc.Get(loca.AWSStatusTitle), resp.Response.Card.Title)
+	assert.Equal(t, loc.Get(loca.AWSStatusAreaElicitText), resp.Response.Card.Content)
+}
+
+func TestLambda_HandleAWSStatus_WithSlots(t *testing.T) {
+	initLocaleRegistry(t)
+
+	app := alfalfa.NewApplication(log.Null, stats.Null)
+
 	r := &alexa.RequestEnvelope{
+		Version: "1.0",
 		Request: &alexa.Request{
 			Locale: "en-US",
 			Type:   alexa.TypeIntentRequest,
 			Intent: alexa.Intent{
-				Name: loca.SaySomething,
+				Name: loca.AWSStatus,
 				Slots: map[string]*alexa.Slot{
-					"AWSArea": {
-						Name:  "Area",
-						Value: "Europa",
-					},
-					"AWSRegion": {
-						Name:  "Region",
-						Value: "Frankfurt",
+					loca.TypeAreaName: {
+						Name:  loca.TypeAreaName,
+						Value: "Europe",
+						// required!
+						Resolutions: &alexa.Resolutions{
+							PerAuthority: []*alexa.PerAuthority{
+								{
+									Authority: "",
+									Status: &alexa.ResolutionStatus{
+										Code: "ER_SUCCESS_MATCH",
+									},
+									Values: []*alexa.AuthorityValue{
+										{
+											Value: &alexa.AuthorityValueValue{
+												Name: "Europe",
+												ID:   "4312d5c8cdda027420c474e2221abc34",
+											},
+										},
+									},
+								},
+							},
+						},
 					},
 				},
 			},
 		},
 	}
+
 	sb := gen.NewSkillBuilder()
 	b := &alexa.ResponseBuilder{}
 	m := lambda.NewMux(app, sb)
+	m = middleware.WithRequestStats(m, app)
+
+	// with translations
+	loc, err := loca.Registry.Resolve("en-US")
+	assert.NoError(t, err)
+	loc.Set(loca.AWSStatusTitle, []string{"Status"})
+	loc.Set(loca.AWSStatusRegionElicitText, []string{"Elicit Region"})
+	loc.Set(loca.AWSStatusRegionElicitSSML, []string{"<speak>Elicit Region<speak>"})
 
 	m.Serve(b, r)
 	resp := b.Build()
 
+	assert.NotEmpty(t, resp)
+	assert.Equal(t, loc.Get(loca.AWSStatusTitle), resp.Response.Card.Title)
+	assert.Equal(t, loc.Get(loca.AWSStatusRegionElicitText), resp.Response.Card.Content)
+
+	// with Region and missing loca
+	r.Request.Intent.Slots[loca.TypeRegionName] = &alexa.Slot{
+		Name:  loca.TypeRegionName,
+		Value: "Frankfurt",
+		// required!
+		Resolutions: &alexa.Resolutions{
+			PerAuthority: []*alexa.PerAuthority{
+				{
+					Authority: "",
+					Status: &alexa.ResolutionStatus{
+						Code: "ER_SUCCESS_MATCH",
+					},
+					Values: []*alexa.AuthorityValue{
+						{
+							Value: &alexa.AuthorityValueValue{
+								Name: "Frankfurt",
+								ID:   "asdf",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
 	assert.Equal(t, "error", resp.Response.Card.Title)
-	assert.Contains(t, resp.Response.Card.Content, loca.SaySomething)
-	assert.Contains(t, resp.Response.OutputSpeech.SSML, "An error occurred")
+	assert.Contains(t, resp.Response.Card.Content, loca.AWSStatusSSML)
+
+	// with Region and loca
+	loc.Set(loca.AWSStatusText, []string{"Everything alright in %s %s"})
+	loc.Set(loca.AWSStatusSSML, []string{"<speak>All good</speak>"})
+
+	m.Serve(b, r)
+	resp = b.Build()
+
+	assert.NotEmpty(t, resp)
+	assert.Empty(t, resp.Response.Card.Content)
+	assert.Equal(t, loc.Get(loca.AWSStatusTitle), resp.Response.Card.Title)
+	assert.Equal(t, loc.Get(loca.AWSStatusText, "Europe", "Frankfurt"), resp.Response.Card.Text)
 }
